fef3401994596daa1268913fae163632f9377275
priest <ami8800@gmail.com>
2024-02-15 19:42:29 +0200

    improve stability

--- FpvApp/FpvApp/settings_files/settings_product.py
+++ FpvApp/FpvApp/settings_files/settings_product.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^g=j)yc%plfwly5d5%^lg=-mejj!ihbb!!-g=80y^o^8epie)a'
+SECRET_KEY = os.getenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
